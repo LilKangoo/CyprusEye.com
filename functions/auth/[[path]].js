b59b36bacc3440fa9a64216ef9e2d7f8ad0c94b1
LilKangoo <raskangur@gmail.com>
2026-01-25 03:05:38 +0200

    up

--- functions/auth/[[path]].js
+++ functions/auth/[[path]].js
@@ -1,6 +1,13 @@
 import { serveStatic } from '../_utils/serveStatic.js';
 
 export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const pathname = url.pathname || '';
+
+  if (pathname.startsWith('/auth/callback')) {
+    return serveStatic(context, '/auth/callback/index.html');
+  }
+
   // zawsze serwuj /auth/index.html dla wszystkiego pod /auth/*
   return serveStatic(context, '/auth/index.html');
 }
