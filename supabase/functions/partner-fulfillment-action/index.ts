da3d0ce6deda52514a7f6211f080f6183c57d22a
LilKangoo <raskangur@gmail.com>
2026-01-26 17:57:18 +0200

    uo

--- supabase/functions/partner-fulfillment-action/index.ts
+++ supabase/functions/partner-fulfillment-action/index.ts
@@ -17,7 +17,7 @@ type PartnerFulfillmentActionRequest = {
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, x-supabase-client-info, apikey, content-type",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, x-supabase-client-info, x-supabase-client-platform, x-supabase-client-runtime, x-supabase-client-runtime-version, apikey, content-type",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
